f11f8e0605446cd48a811968d5f08a767981051c
rezzandre <31849139+rezzandre@users.noreply.github.com>
2022-02-28 19:11:37 +0700

    first upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// add navbar background when scrolled
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+    if (this.scrollY > 500) {
+      $(".arrow-float-container").addClass("show");
+    } else {
+      $(".arrow-float-container").removeClass("show");
+    }
+    if (this.scrollY > 500) {
+      $(".float-container").addClass("show");
+    } else {
+      $(".float-container").removeClass("show");
+    }
+  });
+});
+
+// hide navbar in responsive mode when clicked
+const navLinks = document.querySelectorAll(".nav-item");
+const menuToggle = document.getElementById("navbarNav");
+const bsCollapse = new bootstrap.Collapse(menuToggle, {
+  toggle: false,
+});
+navLinks.forEach((l) => {
+  l.addEventListener("click", () => {
+    if (bsCollapse._isShown()) {
+      bsCollapse.hide();
+    }
+  });
+});
